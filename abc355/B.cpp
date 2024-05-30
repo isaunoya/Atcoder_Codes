4bb25908fe5644a45c954fd27386acb7b80d5322
isaunoya <isaunoya@qq.com>
2024-05-30 12:16:33 +0800

    /

---
+++ abc355/B.cpp
@@ -0,0 +1,75 @@
+#if defined(LOCAL)
+#include <D:/cp/templates/my_template_compiled.hpp>
+#else
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <bits/stdc++.h>
+#define debug(...) 42
+#define rep1(a) for (auto i = 0; i < a; i++)
+#define rep2(i, a) for (auto i = 0; i < a; i++)
+#define rep3(i, a, b) for (auto i = a; i < b; i++)
+#define rep4(i, a, b, c) for (auto i = a; i < b; i += c)
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define pb emplace_back
+using namespace std;
+template <typename T, typename T2> void cmin(T &x, const T2 &y) {
+  x = x < y ? x : y;
+}
+template <typename T, typename T2> void cmax(T &x, const T2 &y) {
+  x = x > y ? x : y;
+}
+template <typename T> using vc = vector<T>;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+mt19937 rng(time(NULL));
+const int INF = 1000000000;
+const ll LNF = 1000000000000000000;
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define fi first
+#define se second
+#endif
+
+void solve() {
+  vc<pii> C;
+  int N;
+  cin >> N;
+  int M;
+  cin >> M;
+  rep(i, N) {
+    int x;
+    cin >> x;
+    C.pb(x, 0);
+  }
+  rep(i, M) {
+    int x;
+    cin >> x;
+    C.pb(x, 1);
+  }
+  sort(all(C));
+  int ok = 0;
+  rep(i, sz(C) - 1) {
+    if (C[i].se == C[i + 1].se && C[i].se == 0) {
+      ok = 1;
+    }
+  }
+  if (ok) {
+    cout << "Yes\n";
+  } else {
+    cout << "No\n";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
